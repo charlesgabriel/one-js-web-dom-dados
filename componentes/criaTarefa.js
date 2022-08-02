1c1fabf593b5d993dcee1ce60d9caf62afd43971
charlesgabriel <charlesgabriel@hotmail.com.br>
2022-08-01 21:39:50 -0300

    implementado função para exibir os dados/implemented function to display the data

--- componentes/criaTarefa.js
+++ componentes/criaTarefa.js
@@ -5,7 +5,6 @@ export const handleNovoItem = (evento) => {
   evento.preventDefault();
   
   const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
-  const lista = document.querySelector('[data-list]');
   const input = document.querySelector('[data-form-input]');
   const valor = input.value;
   
@@ -20,12 +19,9 @@ export const handleNovoItem = (evento) => {
   };
 
   const tarefasAtualizadas = [... tarefas, dados];
-
-  const criaTarefa = Tarefa(dados);
   
   localStorage.setItem("tarefas", JSON.stringify(tarefasAtualizadas));
   
-  lista.appendChild(criaTarefa);
   input.value = " ";
 };
 
